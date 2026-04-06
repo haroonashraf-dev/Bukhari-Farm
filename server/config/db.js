e82b2a887c0738278b616944549b77606af790c4
Mr.Haroon <haroonashraf.dev@gmail.com>
2026-04-06 22:04:53 +0500

    update files

--- server/config/db.js
+++ server/config/db.js
@@ -6,12 +6,6 @@ const connectDB = async () => {
       console.warn('⚠️ MONGO_URI is not defined. Database connection skipped.');
       return;
     }
-    
-    // If already connected, don't connect again (Serverless friendly)
-    if (mongoose.connection.readyState >= 1) {
-      return;
-    }
-
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
